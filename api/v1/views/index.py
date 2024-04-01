3ca804ae69d1693f388046dc34cd715cb54b5e4b
fassal farah <farahfassal32@gmail.com>
2024-04-01 13:50:58 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 '''api status'''
-import models
-from models import storage
-from models.base_model import BaseModel
+from models.base_model import State, User, Amenity, City, Place, Review
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def return_status():
+    '''return status'''
     return jsonify(status='OK')
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def return_stats():
     '''JSON Responses'''
     todos = {
-            'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review
+        'states': State, 'users': User,
+        'amenities': Amenity, 'cities': City,
+        'places': Place, 'reviews': Review
     }
-    for key in todos:
-        todos[key] = storage.count(todos[key])
+    for key, model in todos.items():
+        todos[key] = storage.count(model)
     return jsonify(todos)
+
+
+if __name__ == "__main__":
+    app_views.run(host='0.0.0.0', port=5000)
+
