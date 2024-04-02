e75ff6d40bb488d8ae7540eea2be997dd932343e
iDamina <daminailiya@gmail.com>
2024-04-02 21:43:53 +0100

    updated init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" Init file for views module """
 from flask import Blueprint
 
 
@@ -9,3 +11,6 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
